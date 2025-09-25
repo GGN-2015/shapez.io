a0ef6def195f56eeec6f0349c59c85e4c610df2c
ZhouHe <az_xcv@126.com>
2025-09-25 20:40:58 +0800

    v0.0.2

--- src/js/changelog.js
+++ src/js/changelog.js
@@ -2,7 +2,7 @@ export const CHANGELOG = [
     {
         version: "0.0.2",
         date: "25.09.2025",
-        entries: ["English language support", "next stage showing bug fixed", ""],
+        entries: ["English language support", "next stage showing bug fixed", "ingame hint"],
     },
     {
         version: "0.0.1",
